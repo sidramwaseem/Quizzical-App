160deb461007b467a92373127958269bca549ea9
Sidra Waseem <sidraw24@gmail.com>
2022-07-15 20:58:18 +0500

    Updated API

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   const [quizData, setQuizData] = React.useState([]);
 
   React.useEffect(() => {
-    fetch("https://opentdb.com/api.php?amount=5&type=multiple")
+    fetch("https://opentdb.com/api.php?amount=5&difficulty=medium&type=multiple")
       .then((res) => res.json())
       .then((data) => setQuizData(data.results));
   }, []);
